aeb9d31cb6282b629c75c6c612baf348a40c21d8
Andressa <andressajucoski@gmail.com>
2021-06-13 17:22:15 -0300

    API request with Axios

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,19 @@ import SafeEnvironment from 'ui/components/feedback/SafeEnvironment/SafeEnvironm
 import PageTitle from 'ui/components/data-display/PageTitle/PageTitle'
 import UserInformation from 'ui/components/data-display/UserInformation/UserInformation'
 import TextFieldMask from 'ui/components/inputs/TextFieldMask/TextFieldMask'
-import { Button, Typography, Container } from '@material-ui/core'
+import { Button, Typography, Container, CircularProgress } from '@material-ui/core'
 import { FormElementsContainer, MaidsContainer, MaidsPaper } from 'ui/styles/pages/index.style'
+import useIndex from 'data/hooks/pages/useIndex.page'
 
 export default function Home() {
+  const { 
+    cep, setCep, cepValido,
+    buscarProfissionais, busca,
+    diaristas, restantes,
+    carregando,
+    erro,
+  } = useIndex()
+
   return (
     <div>
       <SafeEnvironment />
@@ -21,57 +30,58 @@ export default function Home() {
             label={'Digite seu CEP'}
             fullWidth
             variant={'outlined'}
+            value={cep}
+            onChange={(event) => setCep(event.target.value)}
           />
-          <Typography color={'error'}>CEP inválido</Typography>
+
+          {erro && <Typography color={'error'}>{erro}</Typography>}
+
           <Button 
             variant={'contained'} 
             color={'secondary'} 
             sx={{width: '220px'}}
+            disabled={!cepValido || carregando}
+            onClick={() => {buscarProfissionais(cep)}}
           >
-            Buscar
+            {carregando ? <CircularProgress size={20} /> : 'Buscar'}
           </Button>
         </FormElementsContainer>
 
-        <MaidsPaper>
-          <MaidsContainer>
-            <UserInformation 
-              name='Andressa'
-              picture='https://github.com/ndressaa.png'
-              rating={5}
-              description={'Pelotas'}
-            />
-            <UserInformation 
-              name='Andressa'
-              picture='https://github.com/ndressaa.png'
-              rating={5}
-              description={'Pelotas'}
-            />
-            <UserInformation 
-              name='Andressa'
-              picture='https://github.com/ndressaa.png'
-              rating={5}
-              description={'Pelotas'}
-            />
-            <UserInformation 
-              name='Andressa'
-              picture='https://github.com/ndressaa.png'
-              rating={5}
-              description={'Pelotas'}
-            />
-            <UserInformation 
-              name='Andressa'
-              picture='https://github.com/ndressaa.png'
-              rating={5}
-              description={'Pelotas'}
-            />
-            <UserInformation 
-              name='Andressa'
-              picture='https://github.com/ndressaa.png'
-              rating={5}
-              description={'Pelotas'}
-            />
-          </MaidsContainer>
-        </MaidsPaper>
+        {busca && (diaristas.length > 0 ?
+          <MaidsPaper>
+            <MaidsContainer>
+              {diaristas.map((item, index) => {
+                return (
+                  <UserInformation 
+                    key={index}
+                    name={item.nome_completo}
+                    picture={item.foto_usuario}
+                    rating={item.reputacao}
+                    description={item.cidade}
+                  />
+                )
+              })}
+            </MaidsContainer>
+
+            <Container sx={{textAlign: 'center'}}>
+              {restantes > 0 && (
+                <Typography sx={{mt: 5}}>
+                ...e mais {restantes} profissiona{restantes > 1 ? 'is' : 'l'} atendem na sua região
+              </Typography>
+              )}
+              <Button variant={'contained'} color={'secondary'} sx={{mt: 5}}>
+                Contartar um profissional
+              </Button>
+            </Container>
+          </MaidsPaper>
+          :
+          (
+            <Typography align={'center'} color={'textPrimary'} >
+              Ainda não temos nenhum profissional disponível em sua região
+            </Typography>
+          )
+        )}
+
       </Container>
 
     </div>
